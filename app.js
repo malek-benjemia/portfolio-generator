f79bc47c9e7a79eb4bb4b7728624c2815d652fbe
MalekBenJemia <malekbenjemia@gmail.com>
2021-02-01 19:01:38 -0500

    first page

---
+++ app.js
@@ -0,0 +1,33 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+/*const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+      console.log(profileDataArr[i]);
+    }
+  };
+  
+printProfileData(profileDataArgs);*/
+
+
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+
+console.log(generatePage(name, github));
